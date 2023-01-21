de36c0665cf5eb75c940acf5a67498c8da33083e
Haru-arp <vr1997@naver.com>
2023-01-21 19:23:17 +0900

    fix: map key

--- delimo-fe/Delimo/src/component/FriendList/FriendList.jsx
+++ delimo-fe/Delimo/src/component/FriendList/FriendList.jsx
@@ -8,9 +8,9 @@ const FriendList = () => {
       <ScrollView
         contentContainerStyle={{paddingBottom: 100}}
         showsVerticalScrollIndicator={false}>
-        {userData.user.map(data => {
+        {userData.user.map((data, index) => {
           return (
-            <View style={Styles.Container}>
+            <View key={index} style={Styles.Container}>
               <FriendListItem userName={data.userName} title={data.title} />
             </View>
           );
@@ -32,26 +32,32 @@ const Styles = StyleSheet.create({
 const userData = {
   user: [
     {
+      id: 1,
       userName: '새로미',
       title: '1일 1감정일기 화이팅',
     },
     {
+      id: 2,
       userName: '모험가',
       title: '몬스터 잡자',
     },
     {
+      id: 3,
       userName: '비니',
       title: '감정일기 어려워',
     },
     {
+      id: 4,
       userName: '동희',
       title: '개발 파이팅',
     },
     {
+      id: 5,
       userName: '동희',
       title: '개발 파이팅',
     },
     {
+      id: 6,
       userName: '동희',
       title: '개발 파이팅',
     },
